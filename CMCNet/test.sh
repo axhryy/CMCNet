54844e37e01c14ff2649a9138e65e1cc6912f466
axhryy <83200508+axhryy@users.noreply.github.com>
2025-01-26 23:40:33 +0800

    Update test.sh

--- CMCNet/test.sh
+++ CMCNet/test.sh
@@ -1,2 +1,5 @@
- python test.py --gpus 1 --model ctcnet --name SPARNet_S16_V4_Attn2D --load_size 128 --dataset_name single --dataroot "/root/autodl-fs/CTCNet-main/image_origin/Celeba_HR"  --pretrain_model_path "/root/autodl-fs/CTCNet-main/epoch150_18000/CTCNet_S16_V4_Attn2D/iter_20000_net_G.pth"  --save_as_dir "/root/autodl-fs/CTCNet-main/result/CMCNet_Celeba_without_mmb"
-
+# On CelebA Test set
+python test.py --gpus 1 --model CMCNet --name CMCNET \
+    --load_size 128 --dataset_name single --dataroot /Test_Celeba \
+    --pretrain_model_path ./pretrain_models/wfen/wfen_best.pth \
+    --save_as_dir results_celeba/cmcnet
